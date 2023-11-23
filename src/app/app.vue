937e2dd1ffaff2ad5c067885edf4b2b15549357a
LongAidenAi <woai032867@gmail.com>
2023-11-24 02:24:07 +0800

    09 请求接口axios

---
+++ src/app/app.vue
@@ -0,0 +1,25 @@
+<template>
+    <h3>{{ name }}</h3>
+    <div v-for="post in posts" :key="post.id">
+        {{ post.title }} - 
+        <small>{{ post.user.name }}</small>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            name: 'dp-node',
+            posts: []
+        }
+    },
+    created() {
+        axios.get('http://localhost:3000/posts').then(response => {
+            console.log(response)
+            this.posts = response.data
+        })
+    }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import {createApp} from 'vue'
+import App from './app/app.vue'
 
 /**
  * 创建应用
  */
-const app = createApp
+const app = createApp(App)
+
+/**
+ * 挂载应用
+ */
+app.mount('#app')
