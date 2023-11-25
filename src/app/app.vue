f1d8ce3d0b7ac34976bfe8525ae371853012551e
LongAidenAi <woai032867@gmail.com>
2023-11-25 09:17:10 +0800

    10.08 退出登录

--- src/app/app.vue
+++ src/app/app.vue
@@ -3,6 +3,7 @@
 
     <div v-if="currentUser">
         <div>{{ currentUser.name }}</div>
+        <button @click="logout">退出登录</button>
     </div>
     <UserLogin 
     v-if="!isLoginIn" 
@@ -71,6 +72,13 @@ export default {
         }
     },
     methods: {
+        logout() {
+            this.token = ''
+            this.currentUser = null
+
+            localStorage.removeItem('tid')
+            localStorage.removeItem('uid')
+        },
         async getCurrentUser(userId) {
             try {
                 const response = await apiHttpClient.get(`/users/${userId}`)
